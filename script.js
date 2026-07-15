6763b9e100e169f574d83334b0494e2c27ff926a
Jonatas Pereira <jonatas.adsti@gmail.com>
2026-07-15 13:21:06 -0300

    Atualizei o js e css booth changes

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 function entrarNoCurso() {
     console.log('Entrou no curso! Seja bem vindo')
-<<<<<<< HEAD
-=======
-}
-
-function sairDoCurso() {
-    console.log("Saiu do curso! Obrigado");
->>>>>>> dev
 }
